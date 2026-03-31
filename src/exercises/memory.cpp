a4700968c61064fca27171cf390d875ee7d991dc
NamNguyen <nam.nguyen19@sotatek.com>
2026-03-31 11:30:02 +0700

    add task json to support window

---
+++ src/exercises/memory.cpp
@@ -0,0 +1,3 @@
+// https://www.reddit.com/r/cs2a/comments/1h4h2nc/whats_the_difference_between_heap_and_stack_in_c/
+///https://freedium-mirror.cfd/https://medium.com/@sofiasondh/what-is-the-difference-between-new-and-malloc-160c0f38f45f
+// https://www.laptrinhdientu.com/2023/11/cpp-dangling-pointer.html
